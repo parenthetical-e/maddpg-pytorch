69eb0440d84aa705a3c9e23cb0ff32c75ea4b611
Erik Peterson <Erik.Exists@gmail.com>
2022-05-01 15:52:14 -0700

    fea: update reward ref calculations to the more eff. RunningStats

--- infoduel_maddpg/utils/normalize_wrappers.py
+++ infoduel_maddpg/utils/normalize_wrappers.py
@@ -6,29 +6,47 @@ import numpy as np
 import gym
 
 
-def clip_reward(rewards, min_reward, max_reward):
-    """Clip rewards"""
-    return np.clip(rewards, min_reward, max_reward)
+class RunningStats:
+    """Computes running mean and standard deviation.
 
+    Adapted from:
+        - https://gist.github.com/fvisin/5a10066258e43cf6acfa0a474fcdb59f
+        - http://mathcentral.uregina.ca/QQ/database/QQ.09.02/carlos1.html
+    """
 
-def fold_change(rewards, reference_reward, bias=0):
-    """Estimate fold-change, with an optional bias."""
-    #
-    # Mostly this bias is there to allow our scheme to work when there
-    # is NO variance in reward. This happens, as an example. in the "CartPole-v1" task.
-    #
-    # If we do not add a bias in cases like this then the reward
-    # will be at zero always, and learning becomes impossible. Technically,
-    # this bias breaks the notion of Fold-change. I need to handle
-    # the corner case of no reward variance SOMEHOW. This bias is the
-    # answer I decided on. Feel free to choose another, dear reader.
-    rewards = (rewards - reference_reward) / reference_reward
+    def __init__(self):
+        self.n = 0.0
 
-    # If the ref is negative, we need to flip the signs
-    # after normalization.
-    rewards = rewards * np.sign(reference_reward)
+    def clear(self):
+        self.n = 0.0
 
-    return rewards + bias
+    def append(self, x, per_dim=True):
+        x = np.array(x).copy().astype("float16")
+        # process input
+        if per_dim:
+            self._update_params(x)
+        else:
+            for el in x.flatten():
+                self._update_params(el)
+
+    def _update_params(self, x):
+        self.n += 1
+        if self.n == 1:
+            self.m = x
+            self.s = 0.0
+        else:
+            prev_m = self.m.copy()
+            self.m += (x - self.m) / self.n
+            self.s += (x - prev_m) * (x - self.m)
+
+    def mean(self):
+        return self.m if self.n else 0.0
+
+    def var(self):
+        return self.s / (self.n) if self.n else 0.0
+
+    def std(self):
+        return np.sqrt(self.variance())
 
 
 class SequentialBuffer:
@@ -42,32 +60,6 @@ class SequentialBuffer:
     def append(self, x):
         x = np.atleast_1d(x)
         self.buffer.append(x)
-
-    def sample(self):
-        return self.buffer
-
-
-class RandomBuffer:
-    def __init__(self, buffer_size):
-        self.buffer_size = int(buffer_size)
-        self.buffer = list()
-        self.count = 0
-
-    def __len__(self):
-        return len(self.buffer)
-
-    def append(self, x):
-        # Force min d and into np
-        x = np.atleast_1d(x)
-
-        # If smaller than buffer size, append
-        if self.count <= self.buffer_size:
-            self.buffer.append(deepcopy(x))
-            self.count += 1
-        # Otherwise make a random insert
-        else:
-            i = np.random.randint(0, self.buffer_size)
-            self.buffer[i] = deepcopy(x)
 
     def sample(self):
         return self.buffer
@@ -116,31 +108,23 @@ class MovingFoldChangeReward(gym.Wrapper):
         intial_reference_reward=0.01,
         # min_reference_reward=0.001,
         bias_reward=0.0,
-        buffer_size=1000,
+        # buffer_size=1000,
     ):
         super().__init__(env)
         self.env = env
 
         # Init
         self.bias_reward = bias_reward
-        # self.min_reference_reward = min_reference_reward
         self.intial_reference_reward = intial_reference_reward
-        self.buffer_size = buffer_size
 
         # Construct independent buffers for all agents
-        # and set initial ref
         self.buffer = {}
         self.reference_reward = {}
         for a in self.possible_agents:
-            # Init
-            # self.buffer[a] = RandomBuffer(self.buffer_size)
-            self.buffer[a] = SequentialBuffer(self.buffer_size)
-            # Fill
-            for _ in range(self.buffer_size):
-                self.buffer[a].append(intial_reference_reward)
-            # Update ref
-            mean_reward = np.mean(self.buffer[a].sample())
-            self.reference_reward[a] = mean_reward
+            self.buffer[a] = RunningStats()
+            # ...and set initial value
+            self.buffer[a].append(intial_reference_reward)
+            self.reference_reward[a] = self.buffer[a].mean()
 
         # Sanity
         assert len(self.reference_reward) == len(
@@ -149,17 +133,19 @@ class MovingFoldChangeReward(gym.Wrapper):
 
     def step(self, actions):
         next_states, rewards, dones, infos = self.env.step(actions)
+        self.update_reference(rewards)
+        rewards = self.normalize(rewards)
+
+        return next_states, rewards, dones, infos
+
+    def update_reference(self, rewards) -> None:
         # Update reward memmory
         for a in rewards.keys():
             self.buffer[a].append(rewards[a])
         # Update reference rewardss
         for a in rewards.keys():
-            mean_reward = np.mean(self.buffer[a].sample())
-            self.reference_reward[a] = mean_reward
-        # Est. fold-change
-        rewards = self.normalize(rewards)
-
-        return next_states, rewards, dones, infos
+            # mean_reward = np.mean(self.buffer[a].sample())
+            self.reference_reward[a] = self.buffer[a].mean()
 
     def normalize(self, rewards):
         fold_rewards = {}
