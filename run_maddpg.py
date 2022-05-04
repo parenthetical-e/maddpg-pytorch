95efc42d71dd850f15c8eebae46171c682ef401b
Erik Peterson <Erik.Exists@gmail.com>
2022-05-03 19:21:06 -0700

    refactor: port run_ to saner device swapping
    that did nothing for performance
    
    ... i odn't understand this code base. it works for know

--- run_maddpg.py
+++ run_maddpg.py
@@ -8,6 +8,7 @@ import os
 import numpy as np
 from gym.spaces import Box, Discrete
 from pathlib import Path
+from tqdm import tqdm
 
 from torch.utils.tensorboard import SummaryWriter
 
@@ -29,8 +30,7 @@ def run(config):
     torch.manual_seed(config.seed)
     np.random.seed(config.seed)
 
-    device = get_device(config.device)
-    if device == "cpu":
+    if config.device == "cpu":
         torch.set_num_threads(config.n_training_threads)
 
     # --- Setup log paths.
@@ -59,12 +59,13 @@ def run(config):
     logger = SummaryWriter(str(log_dir))
 
     print(f"----- Running: {config.env_id} ------")
-    print(f"device: {device}")
+    print(f"device: {config.device}")
     print(f"log_dir: {log_dir}")
 
-    # --- Build the env, an MPE zoo
-    # TODO: add env_hparams as an arg
+    # Set
+    th_device = torch.device(config.device)
 
+    # --- Build the env, an MPE zoo
     Env = getattr(mpe, config.env_id)
     env = Env.parallel_env(continuous_actions=True)
     env = clip_actions_v0(env)
@@ -83,6 +84,7 @@ def run(config):
         tau=config.tau,
         lr=config.lr,
         hidden_dim=config.hidden_dim,
+        device=config.device,
     )
     replay_buffer = ReplayBuffer(
         config.buffer_length,
@@ -102,20 +104,13 @@ def run(config):
     # RUN loop: run a set of episodes (n_episodes), where each has a
     # pre-definited duration (episode_length)
     t = 0
-    for ep_i in range(0, config.n_episodes, config.n_rollout_threads):
-        print(
-            f"Episodes {ep_i + 1}-{ep_i + 1 + config.n_rollout_threads} of {config.n_episodes}"
-        )
-
+    for n in tqdm(range(config.n_episodes), desc=config.env_id):
         # Reset several things:
-        # - env.
-        # - buffers,
-        # - and the exploration noise (if it was used).
         obs = env.reset()
-        maddpg.prep_rollouts(device="cpu")
+        maddpg.prep_rollouts()
 
         explr_pct_remaining = (
-            max(0, config.n_exploration_eps - ep_i) / config.n_exploration_eps
+            max(0, config.n_exploration_eps - n) / config.n_exploration_eps
         )
         maddpg.scale_noise(
             config.final_noise_scale
@@ -124,7 +119,7 @@ def run(config):
         maddpg.reset_noise()
 
         # --- Rollout loop - we do for a fixed length
-        for et_i in range(config.episode_length):
+        for _ in range(config.episode_length):
 
             # If there are no agents the env
             # should be restarted.
@@ -133,7 +128,7 @@ def run(config):
 
             # rearrange observations for maddpg
             torch_obs = [
-                torch.tensor(obs[a], requires_grad=False).unsqueeze(0)
+                torch.tensor(obs[a], requires_grad=False).to(th_device).unsqueeze(0)
                 for a in env.possible_agents
             ]
             # Agents act 'at once' (as parallelized AEC)
@@ -157,52 +152,36 @@ def run(config):
 
             # setup for next step
             obs = next_obs
-            t += config.n_rollout_threads
+            t += 1
 
-            # print("----------------------------")
-            # print("ei_i", et_i)
-            # print("obs.keys():", obs.keys())
-            # print("obs.shape:", list(obs.values())[0].shape)
-            # print("obs:", obs)
-            # print("torch_obs:", torch_obs)
-            # print("actions[0].shape:", list(agent_actions.values())[0].shape)
-            # print("agent_actions:", agent_actions)
-            # print("torch_agent_actions:", torch_agent_actions)
-            # print("dones:", dones)
-            # print("rewards:", rewards)
-            # print("next_obs:", next_obs)
-
-            # train (ugly code)
+            # --- Train (ugly code)?
             if (
                 len(replay_buffer) >= config.batch_size
-                and (t % config.steps_per_update) < config.n_rollout_threads
+                and (t % config.steps_per_update) == 1
             ):
-                maddpg.prep_training(device=device)
-                for u_i in range(config.n_rollout_threads):
-                    for a_i, a_n in enumerate(env.possible_agents):
-                        sample = replay_buffer.sample(config.batch_size, device)
-                        maddpg.update(sample, a_i, a_n, logger=logger)
-                    maddpg.update_all_targets()
-                maddpg.prep_rollouts(device="cpu")
+                maddpg.prep_training()
+                for a_i, a_n in enumerate(env.possible_agents):
+                    sample = replay_buffer.sample(config.batch_size, config.device)
+                    maddpg.update(sample, a_i, a_n, logger=logger)
+                maddpg.update_all_targets()
+                maddpg.prep_rollouts()
 
         # --- Post-episode LOG....
         # Data
-        ep_rews = replay_buffer.get_average_rewards(
-            config.episode_length * config.n_rollout_threads
-        )
+        ep_rews = replay_buffer.get_average_rewards(config.episode_length)
         for i, a in enumerate(env.possible_agents):
             logger.add_scalar(
-                f"{a}/std_episode_actions", replay_buffer.ac_buffs[i].std(), ep_i
+                f"{a}/std_episode_actions", replay_buffer.ac_buffs[i].std(), n
             )
             logger.add_scalar(
-                f"{a}/std_episode_rewards", replay_buffer.rew_buffs[i].std(), ep_i
+                f"{a}/std_episode_rewards", replay_buffer.rew_buffs[i].std(), n
             )
-            logger.add_scalar(f"{a}/mean_episode_rewards", ep_rews[i], ep_i)
+            logger.add_scalar(f"{a}/mean_episode_rewards", ep_rews[i], n)
 
         # Models
-        if ep_i % config.save_interval < config.n_rollout_threads:
+        if n % config.save_interval == 1:
             os.makedirs(run_dir / "incremental", exist_ok=True)
-            maddpg.save(run_dir / "incremental" / ("model_ep%i.pt" % (ep_i + 1)))
+            maddpg.save(run_dir / "incremental" / ("model_ep%i.pt" % (n + 1)))
             maddpg.save(run_dir / "model.pt")
 
     # --- Cleanup/save/etc are done
@@ -218,9 +197,8 @@ if __name__ == "__main__":
         "model_name", help="Name of directory to store " + "model/training contents"
     )
     parser.add_argument("--seed", default=1, type=int, help="Random seed")
-    parser.add_argument("--n_rollout_threads", default=1, type=int)
     parser.add_argument("--n_training_threads", default=6, type=int)
-    parser.add_argument("--buffer_length", default=int(1e6), type=int)
+    parser.add_argument("--buffer_length", default=25000, type=int)
     parser.add_argument("--n_episodes", default=25000, type=int)
     parser.add_argument("--episode_length", default=25, type=int)
     parser.add_argument("--steps_per_update", default=100, type=int)
